de43c3d95aaf139ebf93555834d84ca88f09b657
Qawelesizwe <qawemlilo@gmail.com>
2013-03-22 14:35:46 +0200

    New pages created

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 var http = require('http'),
     connect = require('connect'),
+    redirect = require('connect-redirection'),
     mailer = require('./mailer'),
     port = process.env.PORT || 3002;
     
     
 var app = connect()
+    .use(redirect())
     .use(connect.static('app')) 
-    .use(connect.bodyParser());
-   // .use('/contact', contact);
+    .use(connect.bodyParser())
+    .use('/contact', contact);
+
+
+
+function contact(req, res) {
+    var name = req.body.name, 
+        email = req.body.email,
+        phone = req.body.phone,
+        bugdet = req.body.bugdet,
+        message = req.body.message;
+ 
+    res.redirect('/success.html');
+}
+
+
 
 
 http.createServer(app).listen(port, function() {
   console.log('App running at http://localhost:%s', port);
 });
-
-
-
-function contact(req, res) {
-    var url = req.body.video, 
-        format = req.body.format || 'flv',
-        quality = req.body.quality || '18';
-}
